249e2db9f645ce6bb828b3363ca21a8e04202675
IhorButov <butovigorg@gmail.com>
2022-10-08 10:17:01 +0300

    Update srcipt1.js

---
+++ js/srcipt1.js
@@ -0,0 +1,8 @@
+function calculateEngravingPrice(message, pricePerWord) {
+    // Change code below this line
+ let string;
+   string = (message, pricePerWord).split(" ");
+ return string;
+}
+
+calculateEngravingPrice("JavaScript is in my blood",10)
