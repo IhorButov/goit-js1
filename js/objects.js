01acce23a5dc371a995f6f97b165e0a607819cbe
IhorButov <butovigorg@gmail.com>
2022-10-17 16:37:17 +0300

    Update objects.js

--- js/objects.js
+++ js/objects.js
@@ -1,4 +1,4 @@
-const playlist = {
+/*const playlist = {
     name: 'My playlist',
     rating: 5,
     tracks: ['Track1', 'Track2', 'Track3'],
@@ -24,7 +24,7 @@ playlist.addTrack('new track5');
 playlist.addTrack('new track5');
 playlist.updateRating(10);
 console.log(playlist.getTrackCount());
-console.log(playlist);
+console.log(playlist);*/
 /*
 
 const inputName = 'color';
@@ -33,4 +33,43 @@ const inputValue = 'tomato';
 const colorData = {
     [inputName]: inputValue,
 }
-console.log(colorData);*/
+console.log(colorData);
+const feedback = {
+    good: 5,
+    neutral: 10,
+    bad: 3,
+};
+
+let totalFeedback = 0;
+const keys = Object.keys(feedback);
+console.log(keys);
+for (const key of keys) {
+    console.log(key);
+    console.log(feedback[key]);
+}*/
+
+const friends = [
+    {name: 'Mango',online: false},
+    {name: 'Kiwi',online: true},
+    {name: 'Poly',online: true},
+    {name: 'Ajax',online: false},
+];
+console.table(friends);
+/*
+const findFrienByName = function (allFriends, name) {
+    for (const friend of allFriends) {
+        console.log(friend.name);
+    }
+};
+
+console.log(findFrienByName(friends, 'Poly'))*/
+
+const getAllNames = function (allFriends) {
+    const names = []; 
+    for (const friend of allFriends) {
+        console.log(friend.name);
+        names.push(friend.name);
+    }
+    return names;
+}
+console.log(getAllNames(friends));
