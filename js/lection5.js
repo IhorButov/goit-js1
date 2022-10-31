d038eb2e8cc740f0aceffb1b6a266ffb9bf64109
IhorButov <butovigorg@gmail.com>
2022-10-31 13:45:27 +0200

    Update lection5.js

--- js/lection5.js
+++ js/lection5.js
@@ -1,46 +1,33 @@
-/* => Example 4 - Комплексные задачи
- Напиши скрипт управления личным кабинетом интернет банка.
- Есть объект account в котором необходимо реализовать
- методы для работы с балансом и историей транзакций.
- * Типов транзацкий всего два.* Можно положить либо снять деньги со счета */
- const Transaction = {
-	DEPOSIT: "deposit",
- 	WITHDRAW: "withdraw", };
-/*Каждая транзакция это объект со свойствами: id, type и amount */
+const Transactions = {
+	DEPOSIT: 'deposit',
+	WITHDRAW: 'withdraw',
+};
+
 const account = {
- 	balance: 0, 	 	 //Текущий баланс счета
- 	transactions: [], 	 //История транзакций
- 	/* Метод создает и возвращает объект транзакции.
-	Принимает сумму и тип транзакции.*/
- 	createTransaction(amount, type) {
-		const newTransaction = {
-			id: '',
+	balance: 0,
+
+	transactions: [],
+
+	creatTransaction(amount, type) {
+		this.transactions.push({
+			amount,
 			type,
-			amount: '',
-		}
- 	},
- 	/*Метод отвечающий за добавление суммы к балансу.
- 	 * Принимает сумму танзакции.
- 	 * Вызывает createTransaction для создания объекта транзакции
- 	 * после чего добавляет его в историю транзакций*/
- 	deposit(amount) {
- 	},
- 	/*Метод отвечающий за снятие суммы с баланса.
- 	 * Принимает сумму танзакции.
- 	 * Вызывает createTransaction для создания объекта транзакции
- 	 * после чего добавляет его в историю транзакций.
- 	 * Если amount больше чем текущий баланс, выводит сообщение
- 	 * о том, что снятие такой суммы не возможно, недостаточно средств.*/
- 	withdraw(amount) {
+		});
 	},
- 	/*Метод возвращает текущий баланс*/
- 	getBalance() {
- 	},
- 	/* Метод ищет и возвращает объект транзации по id*/
- 	getTransactionDetails(id) {},
- 	/* Метод возвращает количество средств
- 	 * определенного типа транзакции из всей истории транзакций */
- 	getTransactionTotal(type) {
-	}
+
+	deposit(amount) {
+		this.balance += amount;
+	},
+
+	withdraw(amount) {
+		this.balance -= amount;
+	},
+
+	getBalance() {
+		return this.balance;
+	},
+
+
 }
-account.createTransaction(1000, Transaction.DEPOSIT)
+
+account.deposit(1000);
