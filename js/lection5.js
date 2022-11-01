1ac8654dadf32fcf3cfc1935d1983edf0f1fdacf
IhorButov <butovigorg@gmail.com>
2022-11-01 11:05:27 +0200

    Update lection5.js

--- js/lection5.js
+++ js/lection5.js
@@ -1,33 +1,3 @@
-const Transactions = {
-	DEPOSIT: 'deposit',
-	WITHDRAW: 'withdraw',
-};
-
-const account = {
-	balance: 0,
-
-	transactions: [],
-
-	creatTransaction(amount, type) {
-		this.transactions.push({
-			amount,
-			type,
-		});
-	},
-
-	deposit(amount) {
-		this.balance += amount;
-	},
-
-	withdraw(amount) {
-		this.balance -= amount;
-	},
-
-	getBalance() {
-		return this.balance;
-	},
-
-
+const playlist = {
+	
 }
-
-account.deposit(1000);
