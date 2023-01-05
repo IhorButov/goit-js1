70478be9b419a1e67d943604a11d24662b512636
IhorButov <butovigorg@gmail.com>
2023-01-05 15:51:27 +0200

    1

--- js/objects.js
+++ js/objects.js
@@ -1,25 +1,4 @@
-/*const cart = {
-    items: [],
-    getItems() {},
-    add(product) {
-        this.items.push(product);
-    },
-    remove(productName) {},
-    clearInterval() {},
-    countTotalPrice(productName) {},
-    increaseQuantity(productName) {},
-};
-
-cart.add({naem: 'apple', price: 50});
-cart.add({naem: 'lemon', price: 60});
-cart.add({naem: 'lemon', price: 60});
-cart.add({naem: 'strawberry', price: 110});
-
-console.table(cart.getItems());*/
-
 const user = {
-    name: Ihor,
     age: 34,
 }
-
 console.log(user);
